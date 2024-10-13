835f82d850f675c03c9f4c681ea0ea7866ca06bd
Fredrik Ullman <42198444+Fredrik93@users.noreply.github.com>
2024-10-13 14:36:42 +0200

    Add files via upload

--- freull-0/lab2_freull0/src/MyTestBank.java
+++ freull-0/lab2_freull0/src/MyTestBank.java
@@ -7,11 +7,14 @@ public class MyTestBank
     {
         BankLogic bank = new BankLogic();
         bank.createCustomer("Jim", "Nilsson", "666");
+       
         bank.createCreditAccount("666");
+        bank.deposit("666", 1001, 1000);
 
-        System.out.println(bank.getAccount("666", 1001));
-        bank.withdraw("666", 1001, 10000);
-        System.out.println(bank.getAccount("666", 1001));
+        bank.withdraw("666", 1001, 2000);
+        bank.deposit("666", 1001, 0);
 
+        System.out.println(bank.getCustomer("666"));
+      
     }
 }
